6fc8fe285578f61b377aec23adc559c676d0abbb
RAHUL KHALLAR <khallarrahul@gmail.com>
2023-07-07 15:18:33 -0700

    resolves

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
-#!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    fibonacci_sequence = []
+
+    if length >= 1:
+        fibonacci_sequence.append(0)
+    if length >= 2:
+        fibonacci_sequence.append(1)
+
+    while len(fibonacci_sequence) < length:
+        new_number = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+        fibonacci_sequence.append(new_number)
+
+    print(fibonacci_sequence)
+
+
+print_fibonacci(0)
+print_fibonacci(10)
